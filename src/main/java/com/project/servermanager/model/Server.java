d4bf214919be7a1ca0d4ef87c99455d823bae7ae
kaannkaratass <kaan_03_karatas@hotmail.com>
2023-01-14 19:14:56 +0300

    Models

---
+++ src/main/java/com/project/servermanager/model/Server.java
@@ -0,0 +1,24 @@
+package com.project.servermanager.model;
+
+import com.project.servermanager.enumeration.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Server {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+    @Column(unique = true)
+    @NotNull(message = "ipAdress can not be null")
+    private String ipAdress ;
+    private String name ;
+    private String memory ;
+    private String type ;
+    private String imageUrl ;
+    private Status status ;
+}
