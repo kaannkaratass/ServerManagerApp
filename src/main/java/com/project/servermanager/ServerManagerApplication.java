4a95e76f31c6288b918c9ac8e20b64dbbdf098a4
kaannkaratass <kaan_03_karatas@hotmail.com>
2023-01-14 14:49:29 +0300

    initial commit

---
+++ src/main/java/com/project/servermanager/ServerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.servermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagerApplication.class, args);
+	}
+
+}
