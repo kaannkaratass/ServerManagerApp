297cc54df76d41c36ec164aac85e9d5d3c9e0ca6
kaannkaratass <kaan_03_karatas@hotmail.com>
2023-04-02 19:53:08 +0300

    second commit

---
+++ src/main/java/com/project/servermanager/controller/ServerController.java
@@ -0,0 +1,6 @@
+package com.project.servermanager.controller;
+
+public class ServerController {
+
+
+}
---
+++ src/main/java/com/project/servermanager/model/Response.java
@@ -0,0 +1,24 @@
+package com.project.servermanager.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+    protected LocalDateTime timeStamp ;
+    protected int statusCode ;
+    protected HttpStatus status;
+    protected  String reason ;
+    protected String message ;
+    protected String developerMessage ;
+    protected Map<?,?> data ;
+}
